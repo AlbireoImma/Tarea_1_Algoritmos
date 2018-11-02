fd8d345941ea093b96ff8391b6b8c63ad8bb4ebb
Sebastian Godinez <sebastian.godinez@hotmail.com>
2018-11-02 20:05:17 -0300

    gmp

--- src/test.cpp
+++ src/test.cpp
@@ -1,90 +1,103 @@
 #include <stdio.h>
 #include <iostream>
+#include <gmp.h>
 
 using namespace std;
 
 class Rango{
     private:
-    long unsigned int inicio;
-    long unsigned int largo;
+    long long unsigned int inicio;
+    long long unsigned int largo;
     public:
     Rango();
-    Rango(long unsigned int i, long unsigned int l);
+    Rango(long long unsigned int i, long long unsigned int l);
     ~Rango();
-    long unsigned int getInicio();
-    long unsigned int getLargo();
-    void setInicio(long unsigned int i);
-    void setLargo(long unsigned int l);
+    long long unsigned int getInicio();
+    long long unsigned int getLargo();
+    void setInicio(long long unsigned int i);
+    void setLargo(long long unsigned int l);
     void print();
-    long unsigned int newRango(long unsigned int nivel, long unsigned int num);
+    long long unsigned int newRango(long long unsigned int nivel, long long unsigned int num);
 };
 Rango::Rango(){
     inicio = 0;
     largo = 0;
 }
-Rango::Rango(long unsigned int i, long unsigned int l){
+Rango::Rango(long long unsigned int i, long long unsigned int l){
     inicio = i;
     largo = l;
 }
 Rango::~Rango(){
 
 }
-long unsigned int Rango::getInicio(){
+long long unsigned int Rango::getInicio(){
     return inicio;
 }
-long unsigned int Rango::getLargo(){
+long long unsigned int Rango::getLargo(){
     return largo;
 }
-void Rango::setInicio(long unsigned int i){
+void Rango::setInicio(long long unsigned int i){
     inicio = i;
 }
-void Rango::setLargo(long unsigned int l){
+void Rango::setLargo(long long unsigned int l){
     largo = l;
 }
 void Rango::print(){
     cout << "Inicio: " << inicio << " ,Largo: " << largo << endl;
 }
 
-long unsigned int Rango::newRango(long unsigned int nivel, long unsigned int numero){
-    long unsigned int segmento;
-    long unsigned int multiplo;
+long long unsigned int Rango::newRango(long long unsigned int nivel, long long unsigned int numero){
+    long long unsigned int segmento;
+    long long unsigned int multiplo;
     segmento = largo/nivel;
     multiplo = (numero - inicio) / segmento; //REVISAR
-    
-    /**
-    int n = 0;
-    multiplo = 1;
-    while (numero >segmento*multiplo{
-        multiplo += 1;
-        n++;
-    }
-    partir del inicio en 0
-    **/
 
- 
-    inicio = inicio + segmento*multiplo;
+    inicio = inicio + segmento * multiplo;
     largo = segmento;
     return multiplo;
 }
 
-long unsigned int factorial(long unsigned int x){
-    return (x == 1 ? x : x * factorial(x - 1));
+void factorial(long long unsigned int x, mpz_t resultado){
+    mpz_set_ui(resultado, 1);
+    while(x > 1){
+        mpz_mul_ui(resultado, resultado, x);
+        x = x - 1;
+    }
 }
 
-int main(int argc, char const *argv[])
-{
-    Rango prueba;
-    prueba = Rango(1, factorial(26));
-    long unsigned int guardar;
-    
-    for(int i = 1; i < 27; i++)
-    {
-        cout << i << endl;
-        guardar = prueba.newRango(i, 165432);
-        printf("posicion %lu\n", guardar);
-        prueba.print();
+long long unsigned int factorial(unsigned int n){
+    long long unsigned int res;
+
+    while(n > 1){
+        res = res * n;
+        n--;
     }
-    
-    prueba.print();
+
+}
+
+int main(int argc, char const *argv[]){
+
+    mpz_t fact;
+    mpz_init(fact);
+    factorial(32, fact);
+    mpz_out_str(stdout, 10, fact);
+    mpz_clear(fact);
+    unsigned int x = 32;
+    cout << factorial(x) << endl;
+
+
+    // Rango prueba;
+    // prueba = Rango(1, factorial(26));
+    // long long unsigned int guardar;
+
+    // for(int i = 1; i < 27; i++)
+    // {
+    //     cout << i << endl;
+    //     guardar = prueba.newRango(i, 165432);
+    //     printf("posicion %lu\n", guardar);
+    //     prueba.print();
+    // }
+
+    // prueba.print();
     return 0;
 }
