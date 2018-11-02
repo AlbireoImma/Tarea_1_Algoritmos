834ecd1fda6f8a006e9832c1e0a6e6d7267cd743
Sebastian Godinez <sebastian.godinez@sansano.usm.cl>
2018-11-02 14:49:35 -0300

    agregue contador global

--- src/main.cpp
+++ src/main.cpp
@@ -32,73 +32,57 @@ void previo(string palabra, long unsigned int numero){
     backtracking(palabra, combinado, inicial, numero, 1);
     // cout << combinado << endl;
 }
-
-void permutarHelp(string palabra, string dummy, int indicador){
+int act;
+void permutarHelp(string palabra, string dummy, int indicador, int combinacion){
     int i, largo, lDummy;
     string helper;
     largo = palabra.size();
     lDummy = dummy.size();
 
-
-
-    // condicion de termino
-    if (palabra == dummy) {
-        cout << dummy << endl;
-        return;
-    }
     // inserto la ultima letra
     if(largo == dummy.size()) {
-        cout << dummy << endl;
+        act ++;
+        if (act == combinacion) { // si estoy en la combinacion que quiero, termino
+            cout << dummy << endl;
+            return;
+        }
         return;
     }
     else {
-        
         helper = dummy;
-
-
         // if (lDummy == 0){
         //     dummy.insert(0,1,palabra.at(indicador));
         //     permutarHelp (palabra, dummy, indicador +1);
 
         // }
         //inserta las letras
-        for(i=0; i < lDummy +1 ; i++){
-
-
+        for(i=0; i < lDummy +1 && act != combinacion; i++){
             dummy.insert(i,1,palabra.at(indicador));
-            
             //llamado recursivo
-            permutarHelp (palabra, dummy, indicador +1);
-
-
+            permutarHelp (palabra, dummy, indicador +1, combinacion);
             dummy = helper;
-
         }
     }
 }
 
-void permutar( string palabra){
+void permutar( string palabra, int combinacion){
     int i = 0;
     string dummy;
-    permutarHelp(palabra,dummy,0);
+    permutarHelp(palabra,dummy,0, combinacion);
     return;
 }
 
 int main(int argc, char const *argv[]){
+    char *palabra = new char[32];
+    long unsigned int total, largo;
 
-    string palabra = "laoh";
-    permutar(palabra);
-
-    // char *palabra = new char[32];
-    // long unsigned int total, largo;
-
-    // cin >> total;
-    // for(long unsigned int i = 0; i < total; i++)
-    // {
-    //     cin >> palabra;
-    //     cin >> largo;
-    //     previo(palabra, largo);
-    // }
-    //cout << palabra << endl;
+    cin >> total;
+    for(long unsigned int i = 0; i < total; i++)
+    {
+        cin >> palabra;
+        cin >> largo;
+        act = 0;
+        permutar(palabra, largo);
+    }
     return 0;
 }
